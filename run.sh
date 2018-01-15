fd07542b849acc9c90c92be33f0984c5892d1fbc
Dominic Santos <dominicsantos@toasterltd.com>
2018-01-15 18:01:14 +0000

    fix(start): works with both frankenserver and dev_appserver

--- run.sh
+++ run.sh
@@ -16,7 +16,14 @@ case $1 in
     ;;
 
 "start")
-    python site-packages/dev/frankenserver/python/dev_appserver.py app.yaml
+    if [ -d site-packages/dev/frankenserver/ ]; then
+        APPENGINE_SERVER_DIR=site-packages/dev/frankenserver/python/
+
+    else
+        APPENGINE_SERVER_DIR=site-packages/dev/google_appengine/
+
+    fi
+    python $APPENGINE_SERVER_DIR/dev_appserver.py app.yaml
     ;;
 
 *)
